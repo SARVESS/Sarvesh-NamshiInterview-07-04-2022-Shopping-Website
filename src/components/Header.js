bcf61ff5c0d2582b9bad7a7016ab7f6ddfd7820a
SARVESS <sarveshsri76@gmail.com>
2022-04-07 19:34:02 +0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import  {connect} from  'react-redux'
+export class Header extends Component {
+    componentDidMount() {
+        console.log('this -> categories', this.props.categories);
+    }
+    componentDidUpdate() {
+        console.log('this -> categories', this.props.categories);
+    }
+    render() {
+        return (
+            <div className="row">
+              <div className="col-md-12">
+                   { this.props.categories && (<nav className="navbar  navbar-dark bg-dark ">
+                     <ul className="nav">
+                      {this.props.categories.map((category) => {
+                           return (<li className="nav-item" >{category}</li>)
+                      })}     
+                     </ul>
+                  </nav>) }
+                  <nav className="navbar  navbar-dark bg-dark ">
+                        <ul className="nav">
+                            <li className="nav-item" ><Link to="/" className="nav-link active">Products</Link></li>
+                            <li className="nav-item"><Link to="/carts" className="nav-link">Carts : {this.props.numberCart}</Link></li>
+                        </ul>
+                  </nav>
+              </div>
+            </div>
+        )
+    }
+}
+const mapStateToProps = state =>{
+    return{
+        numberCart:state._todoProduct.numberCart,
+        categories:state._todoProduct.categories || []
+    }
+}
+export default connect(mapStateToProps,null)(Header)
